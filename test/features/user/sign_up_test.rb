b0e27ba9bf959d48a853e82910fd79730b22c123
Michael Becco <e4e6d4d5@gmail.com>
2015-07-03 00:01:29 -0700

    Additional Sign up Validation Test

--- test/features/user/sign_up_test.rb
+++ test/features/user/sign_up_test.rb
@@ -14,8 +14,8 @@ feature "As a User I wish to be able to sign up" do
     page.must_have_content "Email"
     page.must_have_content "Password"
 
-    fill_in "Name", with: users(:vic).name
-    fill_in "Email", with: users(:vic).email
+    fill_in "Name", with: "New User"
+    fill_in "Email", with: "newuser@example.com"
     fill_in "Password", with: "newpassword"
     fill_in "Password confirmation", with: "newpassword"
     click_button "Sign up"
@@ -27,28 +27,71 @@ end
 
 feature "As a site owner I require sign up validation restrictions" do
   scenario "Unhappy Path: Empty Name" do
+    visit new_user_registration_path
+    fill_in "Name", with: ""
+    fill_in "Email", with: "newuser@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+    page.must_have_content "Name can't be blank"
   end
-  scenario "Unahppy Path: Maxed Characters for Name" do
+  scenario "Unhappy Path: Maxed Characters for Name" do
+    visit new_user_registration_path
+    fill_in "Name", with: "Me" * 31
+    fill_in "Email", with: "newuser@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+    page.must_have_content "Name is too long (maximum is 30 characters)"
   end
 
   scenario "Unhappy Path: No Email" do
+    visit new_user_registration_path
+    fill_in "Name", with: "Me"
+    fill_in "Email", with: ""
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+    page.must_have_content "Email can't be blank"
   end
   scenario "Unhappy Path: Maxed Characters for Email" do
+    visit new_user_registration_path
+    fill_in "Name", with: "Me"
+    fill_in "Email", with: "a" * 244 + "example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+    page.must_have_content "Email is invalid"
   end
 
   scenario "Unhappy Path: No Password" do
+    visit new_user_registration_path
+    fill_in "Name", with: "Me"
+    fill_in "Email", with: "me@example.com"
+    fill_in "Password", with: ""
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+    page.must_have_content "Password can't be blank"
+
   end
 
   scenario "Unhappy Path: Maxed Password" do
+    visit new_user_registration_path
+    fill_in "Name", with: "Me"
+    fill_in "Email", with: "me@example.com"
+    fill_in "Password", with: "a" * 129
+    fill_in "Password confirmation", with: "a" * 129
+    click_button "Sign up"
+    page.must_have_content "Password is too long (maximum is 128 characters)"
   end
 
   scenario "Unhappy Path: No Password Confirmation" do
+    visit new_user_registration_path
+    fill_in "Name", with: "Maxed"
+    fill_in "Email", with: "maxed@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: ""
+    click_button "Sign up"
+    page.must_have_content "Password confirmation doesn't match Password"
   end
-
-  scenario "Unhappy Path: Maxed Password Confrimation" do
-  end
-
-  scenario "Unhappy Path: Password and Password Confrimation Dont Match" do
-  end
-
 end
