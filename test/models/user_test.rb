b41d53560862654e92fe13f8a65fc88bc5f84da3
Michael Becco <e4e6d4d5@gmail.com>
2015-07-02 22:40:01 -0700

    Add Test for User Model Validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,11 +3,40 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
 
   def user
-    @user ||= User.new
+    @user ||= users(:vic)
   end
 
   def test_valid
     assert user.valid?
   end
 
+  def test_password_length_validity
+    user.password = "1234567"
+    refute user.valid?
+  end
+
+  test 'password can not be blank' do
+    user.password = ""
+    refute user.valid?
+  end
+
+  def test_maximum_password_validity
+    user.password = "a" * 129
+    refute user.valid?
+  end
+
+  def test_maximum_email_validity
+    user.email = "a" * 244 + "example.com"
+    refute user.valid?
+  end
+
+  def test_user_name_cannot_be_blank
+    user.name = ""
+    refute user.valid?
+  end
+
+  test 'maximum user name is 30' do
+    user.name = "a" * 31
+    refute user.valid?
+  end
 end
