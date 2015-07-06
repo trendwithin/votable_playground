ba3166cdc60bc5295676ff0773d627cc6aa7488c
Michael Becco <e4e6d4d5@gmail.com>
2015-07-06 13:22:41 -0700

    Remove Stray Test Line

--- test/features/user/sign_out_test.rb
+++ test/features/user/sign_out_test.rb
@@ -6,7 +6,6 @@ feature "As a User I Want to be Able to Log Out" do
     fill_in 'Email', with: users(:vic).email
     fill_in 'Password', with: 'password'
     click_button 'Log in'
-    save_and_open_page
     click_link 'Logout'
     page.must_have_content "Signed out successfully"
   end
